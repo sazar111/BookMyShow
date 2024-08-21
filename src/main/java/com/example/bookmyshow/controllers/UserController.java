db1400b773f647b7b1ddd9acd7927b88a1137be4
adhi <adhithyan.pay@gmail.com>
2024-08-21 10:01:49 +0530

    commit 3

---
+++ src/main/java/com/example/bookmyshow/controllers/UserController.java
@@ -0,0 +1,13 @@
+package com.example.bookmyshow.controllers;
+
+import com.example.bookmyshow.dtos.CreateBookingRequestDto;
+import com.example.bookmyshow.dtos.CreateBookingResponseDto;
+
+public class UserLoginController {
+    UserLoginService userLoginService;
+
+    public UserLoginControlle
+    public CreateBookingResponseDto UserLogin(CreateBookingRequestDto createBookingRequestDto){
+
+    }
+}
---
+++ src/main/java/com/example/bookmyshow/services/UserService.java
@@ -0,0 +1,5 @@
+package com.example.bookmyshow.services;
+
+public class UserLoginService {
+    public User userLogin(String email, String password)
+}
